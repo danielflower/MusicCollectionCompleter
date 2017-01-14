055f43d9774e2f0006ca33f5a967adbbd4c9b979
Daniel Flower <git@danielflower.com>
2017-01-14 19:06:26 +0800

    iTunes now has rate limiting, so this has made the app really slow. Added sleeping / single threaded calling to get release info so that it makes only around 20 requests per minute.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -11,7 +11,7 @@ This is rather manual and terrible:
 
 1. Go to `MusicCollectionCompleter\Properties\AssemblyInfo.cs` and update `AssemblyVersion` and `AssemblyFileVersion` to a new version.
 2. Clean and build the project with the `Release` profile.
-3. Commit and changes, and tag the repo: `git tag -a v1.2 -m "Release 1.2"`
+3. Commit all changes, and tag the repo: `git tag -a v1.2 -m "Release 1.2"`
 4. Push the tag: `git push --tags` and go to [the release page on GitHub](https://github.com/danielflower/MusicCollectionCompleter/releases) where the new tag should now be showing.
 5. Edit the release, and upload a zip called `MusicCollectionCompleter-1.2.zip` of the release directory (all files can go in the root of the zip file) and publish the release.
 6. Now go to the `gh-pages` branch and update the download link in `index.html`
--- MusicCollectionCompleter/Services/AlbumLookerUpperer.cs
+++ MusicCollectionCompleter/Services/AlbumLookerUpperer.cs
@@ -17,10 +17,12 @@ namespace MusicCollectionCompleter.Desktop.Services
 
         private readonly HttpClient _httpClient;
         private readonly AlbumJsonParser _parser = new AlbumJsonParser(5);
+        private readonly TaskFactory _taskFactory = new TaskFactory();
+        private readonly Semaphore _semaphore = new Semaphore(1, 1);
 
         public AlbumLookerUpperer()
         {
-            ServicePointManager.DefaultConnectionLimit = 20;
+            ServicePointManager.DefaultConnectionLimit = 1;
             _httpClient = new HttpClient();
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
@@ -29,27 +31,38 @@ namespace MusicCollectionCompleter.Desktop.Services
 
         public void ProcessArtistAsync(Artist artist)
         {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            GetJsonAsync(artist.Name)
-                .ContinueWith(task =>
-                {
-                    stopwatch.Stop();
-                    string duration = stopwatch.ElapsedMilliseconds.ToString("#,###") + "ms";
-                    if (task.Status == TaskStatus.RanToCompletion)
+            _taskFactory.StartNew(() =>
+            {
+                _semaphore.WaitOne();
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                GetJsonAsync(artist.Name)
+                    .ContinueWith(task =>
                     {
-                        Console.WriteLine("Got " + artist.Name + " info in " + duration);
-                        artist.AddAvailableAlbums(_parser.ToAlbums(artist.NormalisedName, task.Result));
-                    }
-                    else
-                    {
-                        Console.WriteLine("Task failed for " + artist.Name + " after " + duration + ": " + task.Status);
-                    }
-                });
+                        stopwatch.Stop();
+                        string duration = stopwatch.ElapsedMilliseconds.ToString("#,###") + "ms";
+                        if (task.Status == TaskStatus.RanToCompletion)
+                        {
+                            Console.WriteLine("Got " + artist.Name + " info in " + duration);
+                            artist.AddAvailableAlbums(_parser.ToAlbums(artist.NormalisedName, task.Result));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Task failed for " + artist.Name + " after " + duration + ": " + task.Status + " - " + task.Exception);
+                            // iTunes rate limits the search API now - as of writing this comment, it's around 20 per minute.
+                            ProcessArtistAsync(artist);
+                        }
+                        Thread.Sleep(TimeSpan.FromSeconds(3));
+                        _semaphore.Release();
+                    });
+            });
         }
 
         public Task<string> GetJsonAsync(string artist)
         {
+            
             string url = ItunesSearchUrl.Replace("{artist-name}", WebUtility.UrlEncode(artist));
+            Console.WriteLine("Calling " + url);
             return _httpClient.GetStringAsync(url);
         }
 
